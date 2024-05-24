3ae1cdf24b48d0f26a89cb5af6c45879fc88d29d
Tanmay0215 <134931600+Tanmay0215@users.noreply.github.com>
2024-05-24 09:06:11 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,57 +1,35 @@
-// create web server
-const http = require('http');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
 const fs = require('fs');
-const url = require('url');
-const querystring = require('querystring');
-const template = require('./lib/template.js');
 const path = require('path');
-const sanitizeHtml = require('sanitize-html');
+const commentsPath = path.join(__dirname, 'comments.json');
+const comments = require('./comments.json');
 
-const app = http.createServer((req, res) => {
-    const _url = req.url;
-    const queryData = url.parse(_url, true).query;
-    const pathname = url.parse(_url, true).pathname;
-    console.log(pathname);
-    
-    if (pathname === '/') {
-        if (queryData.id === undefined) {
-            fs.readdir('./data', (err, filelist) => {
-                const title = 'Welcome';
-                const description = 'Hello, Node.js';
-                const list = template.list(filelist);
-                const html = template.HTML(title, list,
-                    `<h2>${title}</h2>${description}`,
-                    `<a href="/create">create</a>`
-                );
-                res.writeHead(200);
-                res.end(html);
-            });
-        } else {
-            fs.readdir('./data', (err, filelist) => {
-                const filteredId = path.parse(queryData.id).base;
-                fs.readFile(`data/${filteredId}`, 'utf8', (err, description) => {
-                    const title = queryData.id;
-                    const sanitizedTitle = sanitizeHtml(title);
-                    const sanitizedDescription = sanitizeHtml(description);
-                    const list = template.list(filelist);
-                    const html = template.HTML(sanitizedTitle, list,
-                        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-                        `<a href="/create">create</a>
-                         <a href="/update?id=${sanitizedTitle}">update</a>
-                         <form action="delete_process" method="post">
-                             <input type="hidden" name="id" value="${sanitizedTitle}">
-                             <input type="submit" value="delete">
-                         </form>`
-                    );
-                    res.writeHead(200);
-                    res.end(html);
-                });
-            });
-        }
-    } else if (pathname === '/create') {
-        fs.readdir('./data', (err, filelist) => {
-            const title = 'WEB - create';
-            const list = template.list(filelist);
-            const html = template.HTML(title, list, `
-                <form action="/create_process" method="post">
-                    <p><input type="text" name="title"
+app.use(express.static('public'));
+app.use(bodyParser.json());
+
+// get all comments
+app.get('/comments', (req, res) => {
+  res.json(comments);
+});
+
+// add new comment
+app.post('/comments', (req, res) => {
+  if (req.body.comment) {
+    comments.push(req.body);
+    fs.writeFile(commentsPath, JSON.stringify(comments), (err) => {
+      if (err) {
+        res.status(500).send('Error saving comment');
+      } else {
+        res.send('Comment saved');
+      }
+    });
+  } else {
+    res.status(400).send('Comment required');
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Server started on http://localhost:3000');
+});
